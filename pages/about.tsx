d80b0bc10cd79dcac02643ed6f36e47fc2ef5cf0
investgod <110871245+investgod@users.noreply.github.com>
2024-01-25 18:16:30 -0600

    Update README and code changes

--- pages/about.tsx
+++ pages/about.tsx
@@ -15,7 +15,7 @@ export default function About() {
         <h1 className="text-center text-3xl font-bold mb-2 text-blue-600">Meet Bruce Sailes</h1>
         <p className="text-center text-gray-700 text-lg mb-4">Developer | Creator | Artist | Writer</p>
         <p className="text-gray-600">
-          Welcome to my world! I&ldquo;m Bruce Sailes, a versatile web developer, creative thinker, passionate artist, and wordsmith. With a passion for technology and a creative soul, I craft websites, create art, and write to inspire and empower others.
+          Greetings, and welcome to my realm! I am Bruce Sailes, a versatile web developer, a visionary creative, a passionate artist, and a wordsmith. Fueled by my love for technology and guided by my creative spirit, I specialize in crafting websites, producing captivating art, and composing inspirational content, all with the aim of igniting inspiration and empowering individuals like yourself.
         </p>
         <p className="text-gray-600">
           My journey is all about blending the beauty of art and the precision of code to make the digital world more engaging and meaningful. Join me as we explore the realms of creativity and technology on this exciting journey!
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,5 +1,6 @@
 import nodemailer from 'nodemailer';
 
+
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
