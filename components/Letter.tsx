bd11946d63ac1327919bd6ebb90bab5964667995
investgod <110871245+investgod@users.noreply.github.com>
2024-01-26 13:35:26 -0600

    Updates needed

--- components/Letter.tsx
+++ components/Letter.tsx
@@ -5,7 +5,7 @@ interface LetterProps {
   post: {
     content: string;
     title: string;
-    date: string;
+    // date: string;
     slug: string; 
   };
 }
@@ -15,9 +15,9 @@ const Letter: React.FC<LetterProps> = ({ post }) => {
 
   return (
     <div className="bg-white p-4 rounded shadow-md hover:shadow-lg">
-      <h2 className="text-xl font-semibold mb-2">{post.title}</h2>
-      <p className="text-gray-600 mb-2">{post.date}</p>
-      <p className="mb-4">{teaser}...</p>
+       <h2 className="text-xl font-semibold mb-2">{post.title}</h2>
+      {/* <p className="text-gray-600 mb-2">{post.date}</p>
+      <p className="mb-4">{teaser}...</p> */} 
       <Link legacyBehavior href={`/letters/${post.slug}`}>
         <a className="text-blue-500 hover:underline">Read More</a>
       </Link>
