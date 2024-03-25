b4168ed5066cab2f91e4ecf7aa2ac15a8001b367
investgod <110871245+investgod@users.noreply.github.com>
2024-03-25 05:17:05 -0500

    Adding some aesthetic changes to the letters page.

--- components/Letter.tsx
+++ components/Letter.tsx
@@ -15,21 +15,18 @@ interface LetterProps {
 const Letter: React.FC<LetterProps> = ({ post }) => {
  
 
-  const teaser = post.content.substring(0, 100); // Display the first 200 characters as a teaser
-
   return (
-    <div className="bg-white p-4 rounded shadow-md hover:shadow-lg transition-transform transform hover:scale-105">
-       <h2 className="text-xl text-center font-semibold mb-2">{post.Title}</h2>
+    <div className="bg-grey p-4 rounded shadow-md hover:shadow-lg transition-transform transform hover:scale-105">
        {/* Render Image component only if post.Image is not empty */}
        {post.Image && (
          <div className="relative w-full h-80 flex justify-center items-center mb-2">
            <Image className="rounded-2xl" src={post.Image} alt={post.Title} width={250} height={250} />
          </div>
        )}
+      <h1 className="text-gray-600 mb-2">{post.Title}</h1>
       <p className="text-gray-600 mb-2">{post.Date}</p>
-      <p className="mb-4">{teaser}...</p>
       <Link legacyBehavior href={`/letters/${post.Title}`}>
-        <a className="text-blue-500 hover:underline">Read More</a>
+        <a className="text-blue-500 hover:underline">Start Reading</a>
       </Link>
     </div>
   );
